7f1ad930e558582ea4fa76db4421d5dd853757ee
merabi kamjin <merabi@merabi-no-MacBook-Pro.local>
2016-04-19 00:23:56 +0900

    Blood animation added.

--- Assets/Scripts/ParticleSystem/BloodAnimation.cs
+++ Assets/Scripts/ParticleSystem/BloodAnimation.cs
@@ -1,15 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using SmoothMoves;
 
 public class BloodAnimation : MonoBehaviour {
+	public List<string> textureNameList = new List<string> () {
+		"blood1",
+		"blood2",
+		"blood3",
+		"blood4",
+	};
+	public BoneAnimation boneAnimation;
+	public float destroyTime = 2.0f;
+	public float size = 1.0f;
 
-	// Use this for initialization
+	public TextureAtlas bloodAtlas;
+
 	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+		transform.localScale = Vector3.one * size;
+
+		int rnd = Random.Range (0, textureNameList.Count);
+		string replaceId = bloodAtlas.GetTextureGUIDFromName ("blood1");
+		string newTextureId = bloodAtlas.GetTextureGUIDFromName (textureNameList [rnd]);
+		boneAnimation.ReplaceAnimationBoneTexture ("spill", "blood", bloodAtlas, replaceId, bloodAtlas, newTextureId);
+
+		Destroy (gameObject, destroyTime);
 	}
 }
