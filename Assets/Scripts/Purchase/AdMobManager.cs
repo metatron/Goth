fc8ce38dc15196b46e23e2ca55cc114e80b2989c
horikawa <horikawa@MB-430.local>
2016-07-15 19:57:11 +0900

    adding play-services-ads libraries. to do that u need to install the API listed at the PlayerSettings -> minimum API level. after that, run Assets->google play service -> resolve client jars.

---
+++ Assets/Scripts/Purchase/AdMobManager.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+using GoogleMobileAds.Api;
+
+public class AdMobManager : SingletonMonoBehaviourFast<AdMobManager> {
+	public string Android_Banner		= "ca-app-pub-2805884795874004/8649677347";
+	public string Android_Interstitial	= "ca-app-pub-2805884795874004/8649677347";
+
+	public string ios_Banner			= "ca-app-pub-2805884795874004/8649677347";
+	public string ios_Interstitial		= "ca-app-pub-2805884795874004/8649677347";
+
+	private InterstitialAd interstitial;
+	private AdRequest request;
+
+	bool is_close_interstitial = false; 
+
+	// Use this for initialization
+	void Awake () {
+		// 起動時にインタースティシャル広告をロードしておく
+		RequestInterstitial ();
+		// バナー広告を表示
+		RequestBanner ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void RequestBanner() {
+		#if UNITY_ANDROID
+		string adUnitId = Android_Banner;
+		#elif UNITY_IPHONE
+		string adUnitId = ios_Banner;
+		#else
+		string adUnitId = "unexpected_platform";
+		#endif
+
+		// Create a 320x50 banner at the top of the screen.
+		BannerView bannerView = new BannerView(adUnitId, AdSize.Banner, AdPosition.Bottom);
+		// Create an empty ad request.
+		AdRequest request = new AdRequest.Builder()
+			.AddTestDevice(AdRequest.TestDeviceSimulator)       // Simulator.
+			.AddTestDevice("F7D9F6DAF5E046F7028A525895AEAAD7")
+		.Build();
+		// Load the banner with the request.
+		bannerView.LoadAd(request);
+	}
+
+	public void RequestInterstitial() {
+		#if UNITY_ANDROID
+		string adUnitId = Android_Interstitial;
+		#elif UNITY_IPHONE
+		string adUnitId = ios_Interstitial;
+		#else
+		string adUnitId = "unexpected_platform";
+		#endif
+
+		if (is_close_interstitial == true) {
+			interstitial.Destroy ();
+		}
+
+		// Initialize an InterstitialAd.
+		interstitial = new InterstitialAd (adUnitId);
+		// Create an empty ad request.
+		request = new AdRequest.Builder ().Build ();
+		// Load the interstitial with the request.
+		interstitial.LoadAd (request);
+
+		interstitial.OnAdClosed += HandleAdClosed;
+
+		is_close_interstitial = false;
+	}
+
+	// インタースティシャル広告を閉じた時に走る
+	void HandleAdClosed (object sender, System.EventArgs e) {
+		is_close_interstitial = true;
+	}
+}
