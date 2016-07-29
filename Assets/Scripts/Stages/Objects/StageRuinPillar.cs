81e65f2ad5078f21e94343cb4998caae2a304c5b
horikawa <horikawa@MB-430.local>
2016-07-29 13:48:27 +0900

    creating Stage Ruin

---
+++ Assets/Scripts/Stages/Objects/StageRuinPillar.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StageRuinPillar : MonoBehaviour {
+
+	private float rotate = 0.0f;
+
+	void Update () {
+		rotate += Time.deltaTime*2.0f;
+		transform.localRotation = Quaternion.Euler (new Vector3 (0.0f, rotate, 0.0f));
+	}
+}
