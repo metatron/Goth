ce974e88c941edd04a0010ae1267c1b5b7308794
merabi kamjin <merabi@merabi-no-MacBook-Pro.local>
2016-08-17 00:36:53 +0900

    creating RuinHouse events

---
+++ Assets/Scripts/Stages/Objects/StageRuinHouseCross.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StageRuinHouseCross : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other) {
+		if (other.GetComponent<PlayerCharacter> () != null) {
+			GetComponent<BoxCollider> ().enabled = false;
+
+			iTween.ValueTo(gameObject, 
+				iTween.Hash(
+					"from", 0f, 
+					"to", 180f, 
+					"time", 5f, 
+					"onupdate", "UpdateRotateValue",
+					"oncomplete", "OnCompleteRotate",
+					"oncompletetarget", gameObject)
+			);
+		}
+	}
+
+	private void UpdateRotateValue(float delta) {
+		transform.localRotation = Quaternion.Euler (0.0f, 0.0f, delta);
+	}
+
+	private void OnCompleteRotate() {
+	}
+}
