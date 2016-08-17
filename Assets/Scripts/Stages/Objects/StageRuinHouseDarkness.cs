a6cefcb3f2d838190e9b7ed6e3637acfed5dbc0a
horikawa <horikawa@MB-430.local>
2016-08-17 14:13:53 +0900

    creating darkness

---
+++ Assets/Scripts/Stages/Objects/StageRuinHouseDarkness.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StageRuinHouseDarkness : MonoBehaviour {
+
+	void Start () {
+		StartCoroutine (DarknessAction ());
+	}
+
+	private IEnumerator DarknessAction() {
+		//wait for while and enlarge
+		yield return new WaitForSeconds (3.0f);
+
+		iTween.ValueTo(gameObject, iTween.Hash(
+			"from", 1.0f, 
+			"to", 4.0f, 
+			"time", 0.5f, 
+			"onupdate", "UpdateValue",
+			"oncompletetarget", gameObject,
+			"oncomplete", "OnScaleFinished"
+		));
+	}
+
+	private void UpdateValue(float delta) {
+		Vector3 tmpVec = transform.localScale;
+		tmpVec.x = delta;
+		tmpVec.y = delta;
+		tmpVec.z = delta;
+		transform.localScale = tmpVec;
+	}
+
+	private void OnScaleFinished() {
+		Debug.LogError ("******************1");
+	}
+}
