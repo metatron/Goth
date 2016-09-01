55f1f0be987c669f4cabe38edad7cb4dc67d0530
merabi kamjin <merabi@merabi-no-MacBook-Pro.local>
2016-09-02 01:11:44 +0900

    added human animation

---
+++ Assets/Scripts/Stages/Objects/Human.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Human : MonoBehaviour {
+	public GameObject headObj;
+	public GameObject leftArmObj;
+	public GameObject rightArmObj;
+	public GameObject leftLegObj;
+	public GameObject rightLegObj;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
